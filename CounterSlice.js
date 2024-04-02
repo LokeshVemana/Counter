fe0408055cd2c05a1d33d32e309fc74d2c5a9113
LokeshVemana <151106615+LokeshVemana@users.noreply.github.com>
2024-04-02 09:37:49 +0530

    Update CounterSlice.js

--- CounterSlice.js
+++ CounterSlice.js
@@ -1,11 +1,4 @@
-import { createSlice, createAsyncThunk, isRejected } from "@reduxjs/toolkit";
-import axios from "axios";
-
-// export const fetchTodos = createAsyncThunk("Counter/fetchTodos", async () => {
-//   const response = await axios.get("http://localhost:3002/posts");
-
-//   return response.data;
-// });
+import { createSlice } from "@reduxjs/toolkit";
 const CounterSlice = createSlice({
   name: "Counter",
   initialState: {
@@ -22,24 +15,6 @@ const CounterSlice = createSlice({
       state.Counter -= 1;
     },
   },
-  // extraReducers: (builder) => {
-  //   builder
-  //     .addCase(fetchTodos.pending, (state) => {
-  //       state.error = null;
-  //       state.todos = [];
-  //       state.loading = true;
-  //     })
-  //     .addCase(fetchTodos.fulfilled, (state, action) => {
-  //       state.error = null;
-  //       state.todos = action.payload;
-  //       state.loading = false;
-  //     })
-  //     .addCase(fetchTodos.rejected, (state, action) => {
-  //       state.error = action.error.message;
-  //       state.todos = [];
-  //       state.loading = false;
-  //     });
-  // },
 });
 //for dispatch this we need to export//
 export const { increment, decrement } = CounterSlice.actions;
